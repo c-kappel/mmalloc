8d0eb3809f0abe47b7f111a28f2f17ae4d44f508
c-kappel <kcapcode@gmail.com>
2025-04-23 21:57:21 -0400

    implemented a free initialized memory finder so we are not always asking sys for more memory.

--- mmalloc.cpp
+++ mmalloc.cpp
@@ -1,6 +1,30 @@
+#include "mmalloc.h"
+#include "unistd.h"
 #include <iostream>
 
+Block* head = (Block*)sbrk(0);
+
+Block* findFreeInitializedMem(){
+    Block* bottomInitializedMem = head;
+    while(bottomInitializedMem->free == 1){ // occupied
+        bottomInitializedMem = bottomInitializedMem->next;
+    }
+    if(bottomInitializedMem == nullptr){
+        return NULL;
+    }
+    else{
+        
+    }
+    
+}
+
+void* allocate(size_t size){
+    if(size == 0){
+        return NULL;
+    }
+    void* top = sbrk(0);
+}
 
 int main(){
-    sbrk(0);
+
 }
--- mmalloc.h
+++ mmalloc.h
@@ -1,4 +1,14 @@
+#include <iostream>
+#pragma once
 
 struct Block{
-    
+    size_t size;
+    int free; // 0 free, 1 occupied
+    Block* next;
 };
+
+Block* findFreeInitializedMemory();
+
+void* allocate();
+
+void* free();
